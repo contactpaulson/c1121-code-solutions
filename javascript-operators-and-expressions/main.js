147a17094dc4f491178d5b349a6f4a0b9da20e46
Paul Son <contactpaulson@gmail.com>
2021-12-06 13:02:40 -0800

    completed javascript-operators-and-expressions exercise.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,46 @@
+var width = 5;
+var height = 10;
+
+var area = width * height;
+console.log('value of variable area:', area);
+console.log('typeof value of area:', typeof area);
+
+var bill = 100;
+var payment = 200;
+
+var change = payment - bill;
+console.log('value of change:', change);
+console.log('typeof value of change:', typeof change);
+
+var quizzes = 80;
+var midterm = 90;
+var final = 100;
+
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade:', grade);
+console.log('typeof value of grade:', typeof grade);
+
+var firstName = 'Paul ';
+var lastName = 'Son';
+
+var fullName = firstName + lastName;
+console.log('value of fullName:', fullName);
+console.log('typeof value of fullName:', typeof fullName);
+
+var pH = 10;
+
+var isAcidic = pH < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof value of isAcidic:', typeof isAcidic);
+
+var headCount = 10;
+
+var isSparta = headCount === 300;
+console.log('value of isSparta:', isSparta);
+console.log('typeof value of isSparta:', typeof isSparta);
+
+var motto = fullName;
+
+motto = fullName + ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('typeof value of motto:', typeof motto);
