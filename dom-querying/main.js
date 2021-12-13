8f872796cea92a5499fdbb042e650b93f241274a
Paul Son <contactpaulson@gmail.com>
2021-12-13 11:23:33 -0800

    completed dom-querying.

---
+++ dom-querying/main.js
@@ -0,0 +1,17 @@
+console.log('hello, world');
+
+console.log('$heading', document.querySelector('h1'));
+
+console.dir(document.querySelector('h1'));
+
+console.log('$explanation', document.getElementById('explanation'));
+
+console.dir(document.getElementById('explanation'));
+
+console.log('$hint', document.querySelector('.hint'));
+
+console.dir(document.querySelector('.hint'));
+
+console.log('$paragraphs', document.querySelectorAll('p'));
+
+console.log('$link', document.querySelectorAll('.example-link'));
